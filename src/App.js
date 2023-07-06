c86e0eacf3a5b199d05581f11c31d388fce19d90
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-07-06 16:28:29 +0530

    Updating a state array
    The state array updation is different from other elements so we have to make a new array copy the elements and then update it

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with title:", title);
+    const updatedBooks = [
+      ...books,
+      {id:1,title:title}
+    ]
+    setBooks(updatedBooks);
   }
 
   return (
