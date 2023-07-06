9d7a916d83aea2e574ef86d0a5dfb49c13d3e805
Rahulsaraf <63157871+Rahulsaraf1578@users.noreply.github.com>
2023-07-06 16:16:31 +0530

    Creating createBook event handler
    This will be used to pass data between app and book create component. This is passing data from child to parent component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useState } from "react";
+import BookCreate from "./Components/BookCreate";
 
 function App() {
 
   const [books, setBooks] = useState([]);
 
+  const createBook = (title) => {
+    console.log("Need to add book with title:", title);
+  }
+
   return (
-    <div></div>
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
   );
 }
 
